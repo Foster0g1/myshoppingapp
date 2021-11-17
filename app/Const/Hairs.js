5d3e74ec28170d38647ed667f475267024f4a156
Foster0g1 <fmdanfo@st.ug.edu.gh>
2021-11-17 02:53:15 +0100

    First commit

---
+++ app/Const/Hairs.js
@@ -0,0 +1,54 @@
+const Hairs = [
+    {
+      "id": 1,
+      "name": 'Spatula',
+      "price": '39.99',
+      "like": true,
+      "img": require('../assets/ppp.jpg'),
+      "about":'long'
+    },
+  
+    {
+      "id": 2,
+      "name": 'Cooking port',
+      "price": '29.99',
+      "like": false,
+      "img": require('../assets/pp.jpg'),
+      "about":'long'
+    },
+    {
+      "id": 3,
+      "name": 'Frying pan',
+      "price": '25.99',
+      "like": false,
+      "img": require('../assets/dd.jpg'),
+      "about":'long'
+    },
+  
+    {
+      "id": 4,
+      "name": 'Mixers',
+      "price": '25.99',
+      "like": true,
+      "img": require('../assets/lll.jpg'),
+      "about":'long'
+    },
+    {
+      "id": 5,
+      "name": 'Utensils',
+      "price": '50.99',
+      "like": true,
+      "img": require('../assets/ll.jpg'),
+      "about":'long'
+    },
+    {
+      "id": 6,
+      "name": 'Utensils',
+      "price": '50.99',
+      "like": false,
+      "img": require('../assets/ll.jpg'),
+      'about':'long'
+    },
+  ];
+  
+  export default Hairs;
